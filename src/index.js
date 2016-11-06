d692c6b3b3ae21a664c5bf7f752c369780cd9c29
Benjamin Smith <ben.smith@10klt.com>
2016-11-06 14:59:30 -0800

    redux only todo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -13,3 +13,30 @@ ReactDOM.render(
     <App />
   </Provider>
   , document.querySelector('.container'));
+  */
+
+
+
+
+import { addTodo, toggleTodo, setVisibilityFilter, changePerson, VisibilityFilters } from './actions/index';
+import todoApp from './reducers';
+import { createStore } from 'redux';
+
+let store = createStore(todoApp);
+console.log(store.getState());
+
+let unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch(addTodo('set up my redux application'));
+store.dispatch(addTodo('add some react view'));
+store.dispatch(addTodo('add some routing and auth'));
+store.dispatch(toggleTodo(0));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+store.dispatch(changePerson('Benjamin'));
+unsubscribe();
+
+                                  
+
+                                  
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,49 @@
 import { combineReducers } from 'redux';
+import {  VisibilityFilters } from '../actions';
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, PERSON} from '../actions'; 
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
 
-export default rootReducer;
+const SHOW_ALL = VisibilityFilters.SHOW_ALL;
+
+function visibilityFilter(state = SHOW_ALL, action) {
+    switch(action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.filter
+        default:
+            return state    
+    }
+}
+
+function todos(state = [], action) {
+    
+    switch(action.type) {
+        case ADD_TODO:
+            return [...state, { text: action.text, completed: false }]
+        case TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if (index === action.index) {
+                    return Object.assign({}, todo, {completed: !todo.completed})
+                }
+                return todo
+            })    
+            default:
+                return state
+    } 
+}
+
+function creator(state="Ben", action) {
+    switch(action.type) {
+        case PERSON:
+            return action.person
+        default:
+            return state    
+    }
+}
+
+const todoApp = combineReducers({
+    visibilityFilter,
+    todos,
+    creator
+})
+
+export default todoApp;
